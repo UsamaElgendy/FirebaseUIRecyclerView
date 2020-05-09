fde8c65f2dad1e2631eb877f366643f272c35d57
usamaElgendy <48976562+UsamaElgendy@users.noreply.github.com>
2020-05-10 01:48:55 +0200

    implementation

---
+++ app/src/main/java/com/example/firebaseuirecycleradapter/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.firebaseuirecycleradapter;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.firebaseuirecycleradapter.adapter.PostsViewHolder;
+import com.example.firebaseuirecycleradapter.model.Posts;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+
+    private DatabaseReference postsRef;
+    FirebaseRecyclerOptions<Posts> options;
+    FirebaseRecyclerAdapter<Posts, PostsViewHolder> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        recyclerView.setHasFixedSize(true);
+
+        postsRef = FirebaseDatabase.getInstance().getReference().child("Posts");
+
+        options = new FirebaseRecyclerOptions.Builder<Posts>()
+                .setQuery(postsRef, Posts.class).build();
+
+        adapter = new FirebaseRecyclerAdapter<Posts, PostsViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull PostsViewHolder holder, int i, @NonNull Posts model) {
+                Picasso.get().load(model.getImage()).into(holder.imageView, new Callback() {
+                    @Override
+                    public void onSuccess() {
+
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+                holder.textView.setText(model.getName());
+            }
+
+            @NonNull
+            @Override
+            public PostsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_posts_item, parent, false);
+                return new PostsViewHolder(view);
+            }
+        };
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(), 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (adapter != null) {
+            adapter.startListening();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (adapter != null) {
+            adapter.stopListening();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (adapter != null) {
+            adapter.startListening();
+        }
+    }
+}
